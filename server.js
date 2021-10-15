9cf637121fd92c9bd9534c184fe5d991295e7bd2
ykhost <andrew.heringer1337@gmail.com>
2021-10-15 18:02:58 -0300

    Encerrando aposta 04

--- server.js
+++ server.js
@@ -1,1 +1,51 @@
-console.log('comecando')
+const http = require('http')
+const express = require ('express')
+const bodyParser = require('body-parser')
+
+
+
+let contador = 3
+let clientes = [
+    {
+        id: 1,
+        nome: 'joao',
+        email: 'joao@email.com'
+    },
+    {
+        id:2,
+        nome: 'crisitn',
+        email: 'cristiana@email.com'
+    }
+]
+
+const app = express()
+app.use(bodyParser.json())
+const porta = 3000
+app.set('port', porta)
+
+//localhost:3000/cliente (POST)
+app.post('/clientes', (req, res) =>{
+    const cliente = {
+        id: contador++,
+        nome: req.body.nome,
+        email: req.body.email
+    }
+    clientes.push(cliente)
+    res.status(201).json(clientes)
+})
+
+
+//localhost:3000/cliebtes (GET)
+app.get('/clientes',(req, res) =>{
+    res.json(clientes)
+})
+
+//localhost:3000/teste
+app.get('/teste', (req, res) => {
+    console.log("passando por aqui")
+    res.send('ola')
+})
+
+
+const server = http.createServer(app)
+server.listen(porta)
